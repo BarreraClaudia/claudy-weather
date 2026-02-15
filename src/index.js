49a41482109d1996d044242b200a1dfca090e1ed
BarreraClaudia <claudiabarrera.code@gmail.com>
2026-02-15 13:19:51 -0700

    remove webpack icon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import './styles/styles.css';
 import { greeting } from './greeting.js';
-import iconSrc from './assets/imgs/icon.png';
 
 console.log(greeting);
-
-const icon = document.createElement('img');
-icon.src = iconSrc;
-
-document.body.appendChild(icon);
