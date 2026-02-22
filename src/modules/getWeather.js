af60dfde46b62b352ea7bdca11681767af417458
BarreraClaudia <claudiabarrera.code@gmail.com>
2026-02-22 13:36:59 -0700

    add current location button

--- src/modules/getWeather.js
+++ src/modules/getWeather.js
@@ -36,4 +36,40 @@ searchLocationButton.addEventListener('click', async (event) => {
   updateWeatherUI(weatherData);
 });
 
+let currentLocationButton = document.querySelector('.current-location-button');
+
+currentLocationButton.addEventListener('click', handleLocationClick);
+
+async function handleLocationClick() {
+  try {
+    const position = await getPosition();
+    const { latitude: lat, longitude: lng } = position.coords;
+
+    const city = await getCityFromCoords(lat, lng);
+
+    const weatherData = await getWeather(city);
+    updateWeatherUI(weatherData);
+  } catch (error) {
+    console.error('Error getting location or city:', error);
+  }
+}
+
+function getPosition(options = { enableHighAccuracy: true, timeout: 5000 }) {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  });
+}
+
+async function getCityFromCoords(lat, lng) {
+  const apiKey = 'pk.2b7d02841b6e2c346edc5e1da10b7b7e';
+  const url = `https://us1.locationiq.com/v1/reverse.php?key=${apiKey}&lat=${lat}&lon=${lng}&format=json`;
+
+  const response = await fetch(url);
+
+  if (!response.ok) throw new Error('Failed to reverse geocode');
+
+  const data = await response.json();
+  return data.address.city || data.address.town || data.address.village;
+}
+
 export { getWeather };
